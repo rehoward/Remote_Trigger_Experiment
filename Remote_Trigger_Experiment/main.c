c05494917047cce1cbce9be6ae2e99b72b939986
Rich Howard <reh@winlab.rutgers.edu>
2019-09-25 19:24:17 -0400

    Working with simple independent timing for on time of two pump/UV LED pairs. Using 1 MHz calibrated (1%) DCO to drive SMCLK and MCLK.

--- Remote_Trigger_Experiment/main.c
+++ Remote_Trigger_Experiment/main.c
@@ -16,35 +16,60 @@ void main(void)
   
   P1DIR = 0xFF;										// Port 1 all set to output
   P1REN = 0x00;             						// Resistors disabled on inputs
-  P1OUT = (BIT2|BIT4);           					// P1.0 and P1.6 set default low for programmable external trigger
+  P1OUT = 0x00;           							// Port 1 all set low
 		  	  	  	  	  	  	  	  	  	  	  	// P1.2 and P1.4 set high to provide a fixed trigger
 		  	  	  	  	  	  	  	  	  	  	  	// All others set low.
 		  	  	  	  	  	  	  	  	  	  	  	// P1.2/P1.3 and P1.4/P1.5 fixed high trigger.
-  P2DIR = (BIT0|BIT1|BIT4|BIT5);					// P2.0,P2.1,P2.4,P2.5 set to output. All others set to input
-  P2REN = (BIT2|BIT3|BIT6|BIT7);					// P2.2,P2.3,P2.6,P2.7 resistors enabled
+  //P2DIR = (BIT0|BIT1|BIT4|BIT5);					// P2.0,P2.1,P2.4,P2.5 set to output. All others set to input
+  P2DIR = 0xFF;										// Port 2 set to output
+  P2REN = 0x00;										// Port 2 resistor disabled.
   P2OUT = 0x00;										// all P2 resistors set to pulldown and all outputs set low
-  	  	  	  	  	  	  	  	  	  	  	  	  	// P2.0/P2.1 and P2.4/P2.5 provide a fixed low trigger.
+
   P3DIR = 0x00;										// all P3 set to input
   P3REN = 0xFF;										// all P3 input resistors enabled
   P3OUT = 0x00;										// all P3 resistors set to pulldown
   
 
+  P2OUT &= ~BIT0;					//P2.0 low. Motor 2 off
+  P1OUT &= ~BIT5;					//P1.5 UV LED 2 off
+  P1OUT &= ~BIT6;					//Green board LED off
 
+  P2OUT &= ~BIT2;					//P2.2 Low.  Turns off motor 1
+  P2OUT &= ~BIT1;					//P2.1 low.  Turns off UV LED 1
+  P1OUT &= ~BIT0;					//P1.0 low.  Turns off  red LED on board
 
   while(1)
 												//_delay_cycles(10000000) is 80 seconds
   {
+	  //System with two pump drivers and two UV LED drivers, as well as the indicator LEDs on the Launchpad itself.
+	  //LEDs on the board are P1.0 and P1.6.
+	  //Motor 1 (P2.2) on at the same time as UV LED 1 (P2.1) and red board LED (P1.0)
+	  //Motor 2 (P2.0) on at the same time as UV LED 2 (P1.5) and green board LED (P1.6).
 
-			P1OUT |= BIT0;					//P1.0 High and P1.1 low.	Also lights LED
-			_delay_cycles(TENTH_SECOND);	//delay cycle for P1.6/P1.7 to prevent packet collisions.
-			P1OUT |= BIT6;					//P1.6 High and P1.7 low.	Also lights LED
-			_delay_cycles(ON_TIME);  //On time
+	  //Want on time for each UV LED to be 3 hours +/- 30 minutes  Cycle ends 1 hour before sunset and starts at sunrise.
+	  //For this month, good enough is on at 7am and off at 4:45pm.
 
-			P1OUT &= ~BIT0;					//P1.0 low and P1.1 low.	Also turns off LED
-			_delay_cycles(TENTH_SECOND);	//delay cycle for P1.6/P1.7 to prevent packet collisions.
-			P1OUT &= ~BIT6;					//P1.6 low and P1.7 low.	Also turns off LED
+			P1OUT |= BIT0;					//P1.0 High. Lights red LED on board
+			P2OUT |= BIT2;					//P2.2 High.  Turns on motor 1
+			P2OUT |= BIT1;					//P2.1 High.  Turns on UV LED 1
 
-			_delay_cycles(OFF_TIME);         //Off time
+			_delay_cycles(ON_TIME_ONE); 	//On time for Motor1 and UV LED 1
+
+			P2OUT &= ~BIT2;					//P2.2 Low.  Turns off motor 1
+			P2OUT &= ~BIT1;					//P2.1 low.  Turns off UV LED 1
+			P1OUT &= ~BIT0;					//P1.0 low.  Turns off  red LED on board
+
+			P1OUT |= BIT6;					//P1.6 High. Lights  green LED on board
+			P2OUT |= BIT0;					//P2.0 High.  Turns on Motor 2
+			P1OUT |= BIT5;					//P1.5 High.  Turns on UV LED 2
+
+			_delay_cycles(ON_TIME_TWO); 		 //On time for Motor 2  UV LED 2
+
+			P2OUT &= ~BIT0;					//P2.0 Low.  Turns off Motor 2
+			P1OUT &= ~BIT5;					//P1.5 low.  Turns off UV LED 2
+			P1OUT &= ~BIT6;					//P1.6 low.  Turns off green LED on board
+
+
   }
 }
 
--- Remote_Trigger_Experiment/settings.h
+++ Remote_Trigger_Experiment/settings.h
@@ -17,20 +17,22 @@
 
 #define TENTH_SECOND  	     	 12500
 #define ONE_SECOND  	    	125000
+#define TEN_SECOND			   1250000
 #define ONE_HUNDRED_SECOND	  12500000
 #define ONE_HOUR    	 	 450000000
 #define THREE_HOUR			1350000000
 
-//#define ON_TIME 			ONE_SECOND
-//#define ON_TIME 			10*ONE_SECOND
-#define ON_TIME 			ONE_HUNDRED_SECOND
+//#define ON_TIME_ONE		ONE_SECOND
+#define ON_TIME_ONE			10*ONE_SECOND
+#define ON_TIME_TWO			5*ONE_SECOND
+//#define ON_TIME 			ONE_HUNDRED_SECOND
 
 
 //#define OFF_TIME 			ONE_SECOND
-//#define OFF_TIME 			10*ONE_SECOND
+#define OFF_TIME 			10*ONE_SECOND
 //#define OFF_TIME 			ONE_HUNDRED_SECOND
 //#define OFF_TIME 			ONE_HOUR
-#define OFF_TIME 			THREE_HOUR
+//#define OFF_TIME 			THREE_HOUR
 
 
 #endif /* SETTINGS_H_ */

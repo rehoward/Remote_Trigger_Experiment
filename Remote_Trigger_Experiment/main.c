a04166084ed8cf0a5514fcac4dd6238b9656275b
Rich Howard <reh@winlab.rutgers.edu>
2019-09-27 14:21:42 -0400

    Added CdSe photo resistor and a 4.7K resistor to test light  measurement to gate feeding. Also, added 32768 crystal to improve timing accuracy.  Change clock accordingly.

--- Remote_Trigger_Experiment/main.c
+++ Remote_Trigger_Experiment/main.c
@@ -7,14 +7,17 @@
 void main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;     // Stop WDT
+  BCSCTL1 = 0x00;				//XT2 set
+  BCSCTL2 = 0xCB;		//Set MCLK and SMCLK to LFXT1CLK
+  BCSCTL3 = 0x0C;		//set capacitance to 12.5 pFd
 
-  BCSCTL1 = CALBC1_1MHZ;		//DCO set to calibrated 1 MHz
+  //BCSCTL1 = CALBC1_1MHZ;		//DCO set to calibrated 1 MHz
   DCOCTL = CALDCO_1MHZ;			//DCO set to calibrated 1 MHz
   BCSCTL2 = 0x00;
   BCSCTL2 = DIVS_3 +DIVM_3;		//DCO is divided by 8, and sent to SMCLK
 
   
-  P1DIR = 0xFF;										// Port 1 all set to output
+  P1DIR = ~BIT2;									// Port 1 all set to output except P1.2 set to input for light sensor
   P1REN = 0x00;             						// Resistors disabled on inputs
   P1OUT = 0x00;           							// Port 1 all set low
 		  	  	  	  	  	  	  	  	  	  	  	// P1.2 and P1.4 set high to provide a fixed trigger
@@ -46,11 +49,23 @@ void main(void)
 	  //Motor 1 (P2.2) on at the same time as UV LED 1 (P2.1) and red board LED (P1.0)
 	  //Motor 2 (P2.0) on at the same time as UV LED 2 (P1.5) and green board LED (P1.6).
 
-	  //Want on time for each UV LED to be 3 hours +/- 30 minutes  Cycle ends 1 hour before sunset and starts at sunrise.
-	  //For this month, good enough is on at 7am and off at 4:45pm.
+	  //Want on time for each UV LED to be 3 hours +/- 30 minutes  Will use light sensor to turn on and off.
+	  //CdS photoresistor.  Dark over 100K, direct sun cloudy 140 ohms, deep shade 2K ohms.  W
+	  //Will start with 5K ohms as good threshold.
+	  //Set P1.1 high, 4.7K to P1.2 and sensor to P1.2 to P1.3 (set low)
 
-			P1OUT |= BIT0;					//P1.0 High. Lights red LED on board
-			P2OUT |= BIT2;					//P2.2 High.  Turns on motor 1
+	//  	  P1OUT |= BIT0;			//P1.0 High. Lights red LED on board.  P1.3 ground for light sensor.
+
+	  	    P1OUT |= BIT1;					//power to light sensor
+
+	  	    if(P1IN & BIT2)	{				//Check is P1.2 is high => not dark.
+				P1OUT |= BIT0;
+			}
+			else{
+				P1OUT &= ~BIT0;
+			}
+
+/*			P2OUT |= BIT2;					//P2.2 High.  Turns on motor 1
 			P2OUT |= BIT1;					//P2.1 High.  Turns on UV LED 1
 
 			_delay_cycles(ON_TIME_ONE); 	//On time for Motor1 and UV LED 1
@@ -69,7 +84,6 @@ void main(void)
 			P1OUT &= ~BIT5;					//P1.5 low.  Turns off UV LED 2
 			P1OUT &= ~BIT6;					//P1.6 low.  Turns off green LED on board
 
-
-  }
+*/ }
 }
 
